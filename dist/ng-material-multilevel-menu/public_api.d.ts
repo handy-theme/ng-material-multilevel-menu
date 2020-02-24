82de911a16be5d430cae91f46319269eb969d090
Yashar <yyasarr@gmail.com>
2020-02-24 17:45:36 +0300

    only dist folder

--- dist/ng-material-multilevel-menu/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/ng-material-multilevel-menu.module';
-export { MultilevelNodes, Configuration } from './lib/app.model';
--- projects/ng-material-multilevel-menu/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ng-material-multilevel-menu
- */
-
-export * from './lib/ng-material-multilevel-menu.module';
-export { MultilevelNodes, Configuration } from './lib/app.model';
